d8bca4117f938787906e90d540270be2d9d42ca7
dawidtt <dawid.gorszka@gmail.com>
2024-10-14 19:23:34 +0200

    Create function to fetch data from api with given city

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./template.css";
+
+async function getWeatherFromApi(city = "Warszawa") {
+  const weather = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&key=LNU6KDBHHJ4VX7EVPJTQ2E7XZ&contentType=json`,
+  );
+  const weatherJson = await weather.json();
+  console.log(weatherJson);
+  return weatherJson;
+}
+
+async function getNecessaryWeather(weatherJson) {}
+getWeatherFromApi();
