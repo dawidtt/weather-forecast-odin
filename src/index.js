8adfcea2acd5a59894840ae0044282b49489c74d
dawidtt <dawid.gorszka@gmail.com>
2024-10-14 20:54:12 +0200

    Create function to get only needed properties from weather api

--- src/index.js
+++ src/index.js
@@ -9,5 +9,45 @@ async function getWeatherFromApi(city = "Warszawa") {
   return weatherJson;
 }
 
-async function getNecessaryWeather(weatherJson) {}
-getWeatherFromApi();
+async function getNecessaryWeather(weatherJson) {
+  const icon = weatherJson.currentConditions.icon;
+  const description = weatherJson.currentConditions.conditions;
+  const currentTemp = weatherJson.currentConditions.temp;
+  const feelsLike = weatherJson.currentConditions.feelslike;
+  const windspeed = weatherJson.currentConditions.windspeed;
+  const humidity = weatherJson.currentConditions.humidity;
+  const visibility = weatherJson.currentConditions.visibility;
+  const hoursArrToday = weatherJson.days[0];
+  const currentHour = new Date().getHours();
+  const hoursFilteredToday = hoursArrToday.filter((hourObject) => {
+    const reformattedDatetime = hourObject.datetime
+      .split("")
+      .splice(0, 2)
+      .join();
+    if (reformattedDatetime >= currentHour) return true;
+    else return false;
+  });
+  const hoursArrTomorrow = weatherJson.days[1];
+
+  const hoursFilteredTomorrow = hoursArrTomorrow.filter((hourObject) => {
+    const reformattedDatetime = hourObject.datetime
+      .split("")
+      .splice(0, 2)
+      .join();
+    if (reformattedDatetime < currentHour) return true;
+    else return false;
+  });
+  const hours24ArrFiltered = hoursFilteredToday.concat(hoursFilteredTomorrow);
+  return {
+    icon,
+    description,
+    currentTemp,
+    feelsLike,
+    windspeed,
+    humidity,
+    visibility,
+    hours24ArrFiltered,
+  };
+}
+let weatherJson = getWeatherFromApi();
+console.log(weatherJson);
